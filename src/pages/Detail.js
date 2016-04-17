26e765407765d973097e43f07898464a767bcfc5
Fulei Li <DZKXperfect@gmail.com>
2016-04-17 09:19:30 +0800

    use bind

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Chance from 'chance';
 
 class Detail extends React.Component {
+  buttonClicked() {
+    this.forceUpdate();
+  }
   render() {
-    return <p>
-      {this.props.message},
-      {
-        chance.first() == 'Jhon'
-        ? "Hello Jhon"
-        : "Hello world"
-      }
-      </p>
+    return(
+      <div>
+        <p>Hello, {chance.first()}!</p>
+        <p>You're from {chance.country({ full: true })}</p>
+        <button onClick={this.buttonClicked.bind(this)}>Meet Someone New</button>
+      </div>
+    )
   }
 }
 
