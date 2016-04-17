8308de88e21a051939d3a14db8600dbb55996924
Fulei Li <DZKXperfect@gmail.com>
2016-04-17 18:36:51 +0800

    render an array of data with map

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -5,10 +5,16 @@ class Detail extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      name: chance.first(),
-      country: chance.country({ full: true })
+    const people = [];
+
+    for (let i = 0; i < 10; i++) {
+      people.push({
+        name: chance.first(),
+        country: chance.country({ full: name })
+      });
     }
+
+    this.state = { people }
   }
   buttonClicked() {
     const newState = {
@@ -19,9 +25,9 @@ class Detail extends React.Component {
   render() {
     return(
       <div>
-        <p>Hello, { this.state.name }!</p>
-        <p>You're from { this.state.country }</p>
-        <button onClick={this.buttonClicked.bind(this)}>Meet Someone New</button>
+        {this.state.people.map((person, index) => (
+          <p key={index}>Hello, {person.name} from {person.country}!</p>
+        ))}
       </div>
     )
   }
