ce09abfdad57e84614eea9be7b781d8f1df4554a
Fulei Li <DZKXperfect@gmail.com>
2016-04-18 23:13:22 +0800

    read three feeds

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -5,7 +5,12 @@ class Detail extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { commits: [] };
+    this.state = {
+      mode: 'commits',
+      commits: [],
+      forks: [],
+      pulls: []
+    };
   }
 
   componentWillMount() {
@@ -15,23 +20,96 @@ class Detail extends React.Component {
             console.dir(response.body);
             this.setState({ commits: response.body });
           } else {
-            console.log('There was an error fetching from GitHub', error);
+            console.log('Error fetching commits', error);
+          }
+        })
+
+    ajax.get('https://api.github.com/repos/facebook/react/forks')
+        .end((error, response) => {
+          if (!error && response) {
+            console.dir(response.body);
+            this.setState({ forks: response.body });
+          } else {
+            console.log('Error fetching forks', error);
+          }
+        })
+
+    ajax.get('https://api.github.com/repos/facebook/react/pulls')
+        .end((error, response) => {
+          if (!error && response) {
+            console.dir(response.body);
+            this.setState({ pulls: response.body });
+          } else {
+            console.log('Error fetching pulls', error);
           }
         })
   }
 
+  renderCommits() {
+    return this.state.commits.map((commit, index) => {
+      const author = commit.author ? commit.author.login : "Anonymous";
+      return(
+        <p key={index}>
+          <strong>{ author }</strong>:
+          <a href={commit.html_url}>{commit.commit.message}</a>
+        </p>
+      )
+    })
+  }
+
+  renderForks() {
+    return this.state.forks.map((fork, index) => {
+      const owner = fork.owner ? fork.owner.login : "Anonymous";
+      return(
+        <p key={index}>
+          <strong>{ owner }</strong>: forked to
+          <a href={fork.html_url}>{fork.html_url}</a> at {fork.created_at}
+        </p>
+      )
+    })
+  }
+
+  renderPulls() {
+    return this.state.pulls.map((pull, index) => {
+      const user = pull.user ? pull.user.login : "Anonymous";
+      return(
+        <p key={index}>
+          <strong>{ user }</strong>:
+          <a href={pull.html_url}>{pull.body}</a>
+        </p>
+      )
+    })
+  }
+
+  showCommits() {
+    this.setState({ mode: 'commits' })
+  }
+
+  showForks() {
+    this.setState({ mode: 'forks'})
+  }
+
+  showPulls() {
+    this.setState({ mode: 'pulls'})
+  }
+
   render() {
+    let content;
+
+    if (this.state.mode === 'commits') {
+      content = this.renderCommits();
+    } else if (this.state.mode === 'forks') {
+      content = this.renderForks();
+    } else {
+      content = this.renderPulls();
+    }
+
     return(
       <div>
-        {this.state.commits.map((commit, index) => {
-          const author = commit.author ? commit.author.login : "Anonymous";
-          return(
-            <p key={index}>
-              <strong>{ author }</strong>:
-              <a href={commit.html_url}>{commit.commit.message}</a>
-            </p>
-          )
-        })}
+        <button onClick={this.showCommits.bind(this)} name="button">Show Commits</button>
+        <button onClick={this.showForks.bind(this)} name="button">Show Forks</button>
+        <button onClick={this.showPulls.bind(this)} name="button">Show Pulls</button>
+        {content}
       </div>
     )
   }
