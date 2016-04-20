5b1db651e15ee5f62c508c11e626da6e009892d8
Fulei Li <DZKXperfect@gmail.com>
2016-04-20 22:33:06 +0800

    add the link to /react on list page

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class List extends React.Component {
   render() {
-    return <p>This is the list page.</p>
+    return (
+      <div>
+        <p>Please choose a repository from the list below.</p>
+        <ul>
+          <li><Link to="/react">React</Link></li>
+        </ul>
+      </div>
+    )
   }
 }
 
