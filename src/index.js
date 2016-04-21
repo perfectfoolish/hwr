a38f37f3fb4841190104d4accd5cd20569b60b83
Fulei Li <DZKXperfect@gmail.com>
2016-04-21 09:12:16 +0800

    move the routing code out from index.js and in to routes.js so that we have a clear separation of concerns

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { Router, useRouterHistory } from 'react-router';
 import { createHashHistory } from 'history';
 
-import App from './pages/App';
-import List from './pages/List';
-import Detail from './pages/Detail';
+import routes from './routes';
 
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 
 ReactDOM.render(
   <Router history={appHistory} onUpdate={() => window.scrollTo(0, 0)}>
-    <Route path="/" component={ App }>
-      <IndexRoute component={List}/>
-      <Route path="detail/:repo" component={ Detail } />
-    </Route>
+    {routes}
   </Router>,
   document.getElementById('app')
 );
